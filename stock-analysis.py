2328766bc1139232ed756970cdceba5b1d25d269
megc1 <mac492@georgetown.edu>
2019-04-19 00:46:49 -0400

    Necessary imports
    datetime, pandas, quandl, os

---
+++ stock-analysis.py
@@ -0,0 +1,25 @@
+import datetime as dt
+import pandas as pd
+import quandl
+import os
+
+
+#welcome message
+print("Welcome to your portfolio analysis tool!")
+stock_tickers = [] #ticker symbols of stocks in portfolio
+
+ # #Input validation of stock tickers
+ # #TO DO: add validation to look up stock ticker and make sure it exists 
+while True: 
+    ticker_choice = input("Which stock would you like to add to your portfolio? Please enter its ticker symbol or enter \"DONE\" if finished. ")
+    if ticker_choice == "DONE":
+        break
+    elif not ticker_choice.isalpha():
+        print("This doesn't seem to be a valid stock ticker. Please try again!")
+        continue
+    else:
+        stock_tickers.append(ticker_choice)
+
+#TO DO: create summary document
+
+
