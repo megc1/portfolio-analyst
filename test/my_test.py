3040a9e374ce777a6b0738f24033f8c8af5e43a0
megc1 <mac492@georgetown.edu>
2019-04-30 21:03:12 -0400

    sort_growth testing update

--- test/my_test.py
+++ test/my_test.py
@@ -1,1 +1,21 @@
+
 import pytest
+import datetime as dt
+import pandas as pd
+import quandl
+import os
+from dotenv import load_dotenv
+from yahoo_fin.stock_info import get_analysts_info, get_data
+load_dotenv()
+
+from app.stock-analysis import sort_growth
+
+from app.stock-analysis import min_growth
+
+from app.stock-analysis import max_growth
+
+test_list = ["2.5%", "1.0%", "9.5%"]
+def test_sort_growth():
+    result = sort_growth(test_list)
+    assert result == ['1.0 %', '2.5 %', '9.5 %']
+
