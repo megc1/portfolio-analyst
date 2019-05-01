01e0876c37da5e6d3a05284c271ea1b5433b2588
megc1 <mac492@georgetown.edu>
2019-04-30 23:06:48 -0400

    add function imports back

--- test/my_test.py
+++ test/my_test.py
@@ -1,31 +1,10 @@
 import pytest
-import datetime as dt
-import pandas as pd
-import quandl
-import os
-from dotenv import load_dotenv
-import yahoo_fin.stock_info
 
+from app.stock-analysis import sort_growth
 
-# from app.stock-analysis import sort_growth
+from app.stock-analysis import min_growth
 
-# from app.stock-analysis import min_growth
-
-# from app.stock-analysis import max_growth
-
-def sort_growth(a_list):
-    a_list.sort(key = lambda x: float (x[:-1]))
-    return a_list
-
-#gets smallest growth percentage
-def min_growth(b_list):
-    min_value = sort_growth(b_list)[0]
-    return min_value
-
-#gets largest growth percentage
-def max_growth(c_list):
-    max_value = sort_growth(c_list)[-1]
-    return max_value
+from app.stock-analysis import max_growth
 
 test_list = ["2.5%", "1.0%", "9.5%"]
 def test_sort_growth():
